e5cc164c42ad1fa98d3382592608cbe70ae1414e
scastineyras <scastineyras@opengov.com>
2024-04-09 08:31:24 -0300

    using SOLR instead of database Session

--- ckanext/sitemap/view.py
+++ ckanext/sitemap/view.py
@@ -1,42 +1,20 @@
 import logging
 from datetime import datetime, timezone
+from ckan.plugins import toolkit
 import os
 
 from flask import Blueprint, make_response
-from ckan.model import Session, Package
 
 import ckan.plugins.toolkit as tk
 from lxml import etree
 
 sitemap = Blueprint("sitemap", __name__)
 
-# cache = Cache(current_app, config={'CACHE_TYPE': 'simple'})
-
 SITEMAP_NS = "http://www.sitemaps.org/schemas/sitemap/0.9"
 
 XHTML_NS = "http://www.w3.org/1999/xhtml"
 
 log = logging.getLogger(__file__)
-
-defualt_locals = tk.config.get("ckan.locale_default", "en")
-
-if isinstance(defualt_locals, str):
-    defualt_locals = [defualt_locals]
-
-
-def _create_language_alternatives(link, url):
-    """
-    Create links (elements) for every language in locales_offered in .ini file
-    :param link: string containing the link, eg. /dataset/xyz
-    :param url: root node
-    """
-    for lang in defualt_locals:
-        attrib = {
-            "rel": "alternate",
-            "hreflang": lang,
-            "href": tk.config.get("ckan.site_url") + "/" + lang + link,
-        }
-        etree.SubElement(url, "{http://www.w3.org/1999/xhtml}link", attrib)
 
 
 def sitemap_controller():
@@ -54,13 +32,18 @@ def sitemap_controller():
 
     def _create_file(filename, root):
         log.info("Creating new sitemap.xml file: %s", filename)
-        pkgs = (
-            Session.query(Package)
-            .filter(Package.type == "dataset")
-            .filter(Package.private != True)
-            .filter(Package.state == "active")
-            .all()
-        )
+        
+        context = {'model': toolkit.model, 'session': toolkit.model.Session, 'user': toolkit.c.user or toolkit.c.author}
+        data_dict = {
+            'q': '*:*',
+            'start': 0,
+            'rows': 500  # Adjust as needed
+        }
+
+        # Use the package_search action to query datasets
+        search_action = toolkit.get_action('package_search')
+        search_results = search_action(context, data_dict)
+        datasets = search_results['results']
 
         all_ckan_urls = [
             tk.url_for(controller="home", action="index", _external=True),
@@ -73,16 +56,14 @@ def sitemap_controller():
             url = etree.SubElement(root, "url")
             loc = etree.SubElement(url, "loc")
             loc.text = _url
-            _create_language_alternatives(_url, url)
 
-        for pkg in pkgs:
+        for pkg in datasets:
             url = etree.SubElement(root, "url")
             loc = etree.SubElement(url, "loc")
             pkg_url = tk.url_for(controller="dataset", action="read", id=pkg.name)
             loc.text = tk.config.get("ckan.site_url") + pkg_url
             lastmod = etree.SubElement(url, "lastmod")
             lastmod.text = pkg.metadata_modified.strftime("%Y-%m-%d")
-            _create_language_alternatives(pkg_url, url)
             for res in pkg.resources:
                 url = etree.SubElement(root, "url")
                 loc = etree.SubElement(url, "loc")
@@ -94,16 +75,6 @@ def sitemap_controller():
                     resource_id=res.id,
                 )
                 lastmod = etree.SubElement(url, "lastmod")
-                _create_language_alternatives(
-                    tk.url_for(
-                        controller="dataset_resource",
-                        action="read",
-                        id=pkg.name,
-                        package_type=tk.h.default_package_type(),
-                        resource_id=res.id,
-                    ),
-                    url,
-                )
                 lastmod.text = res.created.strftime("%Y-%m-%d")
 
         with open(os.path.join(current_dir, filename), "wb") as f:
@@ -139,7 +110,6 @@ def sitemap_controller():
 
 
 sitemap.add_url_rule("/sitemap.xml", view_func=sitemap_controller, methods=["GET"])
-
 
 def get_blueprints():
     return [sitemap]
